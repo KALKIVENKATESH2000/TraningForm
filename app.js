daaafd1b48002d2a0cc9846f43537547a5332c87
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-12-27 17:17:44 +0530

    updated

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log(`Connected to mongo at ${mongo_url}`));
 // app.use(express.json());
 
+app.use(express.json());
 app.use('/api/form', FormController);
 
 
