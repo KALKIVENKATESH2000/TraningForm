a7c0f2245af99bae9ae67a87c05c628ca3cdcce0
KALKIVENKATESH2000 <kalkivenkateshdonga@gmail.com>
2023-12-28 19:45:22 +0530

    updated

--- app.js
+++ app.js
@@ -3,11 +3,48 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+// const AdminJS = require("adminjs");
+// const AdminJSExpress = require('@adminjs/express')
+// const AdminJSMongoose = require('@adminjs/mongoose')
 const PORT = 3035;
 const { mongo_url } = require('./config/db');
 const FormController = require('./controllers/FormController')
 
+
+const FormModel = mongoose.model('Form');
+
+// AdminJS.registerAdapter({
+//     Resource: AdminJSMongoose.Resource,
+//     Database: AdminJSMongoose.Database,
+// })
+
+// const adminOptions = {
+//     resources: [FormModel],
+// }
+// const admin = new AdminJS(adminOptions)
+// const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+//     admin,
+//     {
+//         cookieName: "adminjs",
+//         cookiePassword: "complicatedsecurepassword",
+//         authenticate: async (email, password) => {
+//             const user = await AdminModel.findOne({ email });
+//             if (user) {
+//                 const matched = await bcrypt.compare(password, user.password);
+//                 if (matched) {
+//                     return true;
+//                 }
+//             }
+//             return false;
+//         },
+//     },
+//     null,
+//     {
+//         resave: false,
+//         saveUninitialized: true,
+//     }
+// );
+// app.use(admin.options.rootPath, adminRouter)
 
 app.use(cors({ origin:'*'}));
 app.use(bodyParser.urlencoded({ extended: false }));
